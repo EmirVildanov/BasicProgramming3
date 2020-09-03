7993141afb27d3f0ed607a4d9b01362b5a857e88
EmirVildanov <reddog201030@gmail.com>
2020-09-03 21:19:56 +0300

    Created project

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    kotlin("jvm") version "1.3.70"
+    id("io.gitlab.arturbosch.detekt") version "1.6.0"
+    id("org.openjfx.javafxplugin") version "0.0.8"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter {
+        content {
+            includeGroup("org.jetbrains.kotlinx")
+        }
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    implementation("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    implementation("no.tornado:tornadofx:1.7.20")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.6.0")
+}
+
+detekt {
+    failFast = true // fail build on any finding
+    buildUponDefaultConfig = true // preconfigure defaults
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    test {
+        useJUnitPlatform()
+    }
+}
